90afc4c6c23ff3df63d510d52b2c007518535c75
Vedidhameliya11 <168515961+Vedidhameliya11@users.noreply.github.com>
2024-05-01 16:02:45 +0530

    Add files via upload

---
+++ vedi7.py
@@ -0,0 +1,83 @@
+# def hello(x = 12,y = 8):
+#     print(x+y)
+#     print(x-y)
+#     print(x*y)
+#     print(x/y)
+#     print(x%y)
+#     print(x**y)
+#     print(x//y)
+# hello()
+    
+# def lotus(x=[1,3,5,7,9],y=[2,4,6,8,10]):
+#     print(x,y)
+# lotus()
+
+# def rose(x=10,y=10):
+#     if x != y:
+#         print("rgh")
+#     else:
+#         print("wrg")
+# rose()
+
+# def sunflwr(v=7,j=18):
+#     if v == j:
+#         print("rgh")
+#     else:
+#         print("wrg")
+# sunflwr()
+
+# def hello(i=1):
+#     while i<5:
+#         print(i)
+#         i = i+0
+# hello()
+
+# def hello(i=2):
+#     while i<10:
+#         print(i)
+#         i = i+1
+#         if i == 8:
+#             break
+# hello()
+
+# def hello(i=3):
+#     while i<12:
+#         i = i+1
+#         if i == 10:
+#             continue
+#         print(i)
+# hello()
+
+# def hello(i = 1):
+#     while i<6:
+#         print(i)
+#         i = i+1
+#      else:
+#          print("hii")
+# hello()
+
+# def hello(a = [1,2,3,4,5]):
+#     for b in a:
+#         print(b)
+#         if b == 4:
+#             break
+# hello()
+
+# def hello(a = ["cat","dog","goat","cow"]):
+#     for b in a:
+#         if b == "goat":
+#             continue
+#         print(b)
+# hello()
+
+# def hello():
+#     for x in range(7):
+#         print(x)
+# hello()
+
+def hello():
+    for x in range(9):
+        print(x)
+    else:
+        print("hello")
+hello()
