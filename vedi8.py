d1e24082f361da773720c6f2cae2d4b42fd11488
Vedidhameliya11 <168515961+Vedidhameliya11@users.noreply.github.com>
2024-05-02 15:57:44 +0530

    Add files via upload

---
+++ vedi8.py
@@ -0,0 +1,220 @@
+# i = 5
+
+# while i<20:
+#     print(i)
+#     i = i+0
+
+# i = 6
+
+# while i<21:
+#     print(i)
+#     i = i+1
+#     if i == 18:
+#         break
+
+# i = 7
+
+# while i<22:
+#     i = i+1
+#     if i == 20:
+#         continue
+#     print(i)
+
+# i = 8
+
+# while i<23:
+#     print(i)
+#     i = i+1
+# else:
+#     print("hello world")
+
+# v = [1,2,3,4,5,6,7]
+
+# for b in v:
+#     print(b)
+#     if b == 3:
+#         break
+
+# a = ["cow","goat","lion","fox"]
+
+# for b in a:
+#     if b == "fox":
+#         continue
+#     print(b)
+
+
+# for x in range(5,30):
+#     print(x)
+
+# for x in range(7,49):
+#     print(x)
+# else:
+#     print("apple")
+
+# x = 12 
+# y = 2
+
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x%y)
+# print(x**y)
+# print(x//y)
+
+# x = 67
+# print(x)
+
+# x += 3
+# print(x)
+
+# x -= 3
+# print(x)
+
+# x = 45
+# y = 78
+
+# if x != y:
+#     print("rgh")
+# else:
+#     print("wrg")
+
+# j = 12
+# v = 77
+
+# if j<v:
+#     print("wrg")
+# else:
+#     print("rgh")
+
+# x = 10
+# y = 10
+
+# if x <= y:
+#     print("rgh")
+# else:
+#     print("wrg")
+
+# x= 4
+# y =6
+
+# print(x>y and y>x and y==x)
+# print(x<y or x==y or x>y)
+# print(not(x>y and x==y or y>x))
+
+
+# x = 12
+# y = 24
+
+# print(x is not y,x != y)
+# print(x is y, x == y)
+
+# x = [40,50,60,70,80,90,100]
+# print(70 not in x)
+
+# x = 10
+# y =15
+
+# print(bin(x))
+# print(bin(y))
+# # print(bin(x&y))
+# print(bin(x|y))
+# print(bin(x^y))
+
+# x = 17
+# y = f'my age is {x}'
+# print(y)
+
+# x = "v is not equal to j"
+# print(x.upper())
+
+# g = "B IS EQUAL TO B"
+# print(g.lower())
+
+# s = "walt disney made disneyland..."
+# print(s.strip())
+
+# a = "disneyland is the most beautiful place on the earth"
+# print(a.find('b'))
+
+# v = "negative thinking"
+# print(v.replace('negative','positive'))
+
+# y = """
+# fhjglkjgf
+# dhfoidjfdk
+# dfjoicsdoivc
+
+
+# """
+# print(y)
+
+# v = "my name is vedi"
+# print(len(v))
+# print(v[:-5:-1])
+
+# x = int(34.567)
+# print(x)
+
+# y = float(56)
+# print(y)
+
+# z = str("how are you")
+# print(z)
+
+# w = complex(77)
+# print(w)
+
+# import random
+# x = random.randrange(1,30)
+# print(x)
+
+# print(complex(3j*4j))
+
+# x = {1,12,123,1234,12345,1234567}
+
+# x.add(12345678)
+# print(x)
+
+# x.copy()
+# print(x)
+
+# x.discard(1234)
+# print(x)
+
+# x.clear()
+# print(x)
+
+# x.pop()
+# print(x)
+
+# x.update({1234,123456789})
+# print(x)
+
+# x = {1:"aemain",2:"keton",3:"aldyhyde",4:"carboxilic acid",5:"alchohol"}
+
+# y=x.keys()
+# print(y)
+
+# y=x.values()
+# print(y)
+
+# y=x.items()
+# print(y)
+
+# x.update({1:"benzin"})
+# print(x)
+# print(type(x))
+
+# d = [12,24,36,"hello","lion","black penthor","tiger",545.6778,23.45667,90.78676]
+
+# print(d[::-1])
+# print(type(d))
+# print(d[1:3])
+
+a = b = c = "rjsjjvj"
+
+print(a)
+print(b)
+print(c)
+
