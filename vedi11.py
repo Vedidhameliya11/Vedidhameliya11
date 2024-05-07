73b5db044267e5da86967e8c666da6873208a424
Vedidhameliya11 <168515961+Vedidhameliya11@users.noreply.github.com>
2024-05-07 15:30:20 +0530

    Add files via upload

---
+++ vedi11.py
@@ -0,0 +1,91 @@
+# def vedi(jk,v):
+#     rm = jk*v
+#     print(rm)
+# vedi(7,56)
+
+# def suga(jin,jimin):
+#     result = jin+jimin
+#     return result
+# jhope = suga(45,78)
+# print(jhope)
+
+# def rm(v):
+#     for lisa in v:
+#         print(lisa)
+# jk = ["rose","iu","jisoo"]
+# rm(jk)
+
+# def demo(v):
+#     return v*7
+# print(demo(18))
+
+# vedi = lambda jk:jk+678
+# print(vedi(45))
+
+# rose = lambda jk,v,jimin:jk+v+jimin
+# print(rose(2,4,6))
+
+# def vedi(num):
+#     return lambda a:a+num
+# jk = vedi(7)
+# print(jk(18))
+
+# def jin(v):
+#     return lambda jk:jk+v
+# suga = jin(5)
+# suga7 = jin(7)
+# print(suga(7))
+# print(suga7(9))
+
+#array in python
+
+# from array import *
+
+# vedi = array('i',[1,2,3,4,5,6,7,8,9,10])
+# print(vedi)
+# print(vedi.buffer_info())
+# print(vedi.typecode)
+# vedi.reverse()
+# print(vedi)
+
+# for v in range(7):
+#     print(vedi[v])
+
+# for v in range(len(vedi)):
+#     print(vedi[v])
+
+# print(vedi[3])
+
+# from array import *
+
+# jk = array('f',[1.2,7.7,8.90,10.345])
+# print(jk)
+# print(jk.buffer_info())
+# print(jk.typecode)
+# jk.reverse()
+# print(jk)
+
+# for j in range(len(jk)):
+#     print(jk[j])
+
+# for s in range(3):
+#     print(jk[s])
+
+# print(jk[3])
+
+from array import *
+
+lisa = array('u',['v','e','d','i'])
+# print(lisa)
+# print(lisa.buffer_info())
+# print(lisa.typecode)
+# print(lisa[3])
+# lisa.reverse()
+# print(lisa)
+
+for t in range(len(lisa)):
+    print(lisa[t])
+
+for t in range(3):
+    print(lisa[t])
+
