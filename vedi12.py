9f3a8164bf014f0315ce87f789c35de1d8790c23
Vedidhameliya11 <168515961+Vedidhameliya11@users.noreply.github.com>
2024-05-08 14:39:57 +0530

    Add files via upload

---
+++ vedi12.py
@@ -0,0 +1,48 @@
+from array import *
+
+# vedi = array('i',[1,2,3,4,5,6,7,8,9,10])
+# print(vedi)
+# print(vedi.buffer_info())
+# print(vedi.typecode)
+# vedi.reverse()
+# print(vedi)
+# print(vedi[6])
+
+# for v in range(len(vedi)):
+#     print(vedi[v])
+
+# for i in range(7):
+#     print(vedi[i])
+
+
+# jk = array('u',['j','h','o','p','e'])
+# print(jk)
+# print(jk.buffer_info())
+# print(jk.typecode)
+# jk.reverse()
+# print(jk)
+# print(jk[4])
+
+# for v in range(5):
+#     print(jk[v])
+
+# for t in range(len(jk)):
+#     print(jk[t])
+
+jisoo = array('f',[1.1,1.2,1.3,1.4,1.5,1.6,1.7])
+# print(jisoo)
+# print(jisoo.buffer_info())
+# print(jisoo.typecode)
+# jisoo.reverse()
+# print(jisoo)
+
+# print(jisoo[5])
+
+for v in range(4):
+    print(jisoo[v])
+
+for e in range(len(jisoo)):
+    print(jisoo[e])
+
+
+
