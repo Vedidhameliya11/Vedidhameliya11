18733be2a0cfcb6f6e741eb89e414ba013bc4706
Vedidhameliya11 <168515961+Vedidhameliya11@users.noreply.github.com>
2024-05-13 16:03:53 +0530

    Add files via upload

---
+++ vedi.py
@@ -0,0 +1,117 @@
+# class vedi:
+#     def jk(self):
+#         print("hello")
+
+# apple = vedi()
+# apple.jk()
+
+# class suga:
+#     def v(self):
+#         print("my name is vedi")
+       
+
+# j = 7
+# print(j)
+
+# com1 = suga()
+
+# com1.v()
+# print(type(com1))
+
+# class rm:
+#     def lisa(self):
+#         print("hello world")
+
+# home = rm()
+
+# home.lisa()
+# print(type(home))
+
+# class vedi:
+#     def jk(self):
+#         print("hii")
+#         i = 1
+#         while i<5:
+#             print(i)
+#             i = i+0
+# apple = vedi()
+# apple.jk()
+
+# class vedi:
+#     def jk(self):
+#         print("my name is vedi")
+#         i = 1
+#         while i<8:
+#             print(i)
+#             i = i+1
+#             if i == 8:
+#                 break
+# mango = vedi()
+# mango.jk()
+
+# class vedi:
+#     def jk(self):
+#         print("national fruit is mango")
+#         i = 2
+#         while i<9:
+#             i = i+1
+#             if i == 6:
+#                 continue
+#             print(i)
+
+# home = vedi()
+# home.jk()
+
+# class vedi:
+#     def jk(self):
+#         print("hello world")
+#         i = 3
+#         while i<11:
+#             print(i)
+#             i = i+1
+#         else:
+#             print("cat")
+# dog = vedi()
+# dog.jk()
+
+# class vedi:
+#     def jk(self):
+#         print("earth india")
+#         a = [1,2,3,4,5,6,7]
+
+#         for b in a:
+#             print(b)
+#             if b == 6:
+#                 break
+# cow = vedi()
+# cow.jk()
+
+# class vedi:
+#     def jk(self):
+#         print("hello world")
+#         a = [12,24,36,48,60]
+#         for b in a:
+#             if b == 48:
+#                 continue
+#             print(b)
+# home = vedi()
+# home.jk()
+
+# class vedi:
+#     def jk(self):
+#         print("hiii")
+#         for x in range(2,50):
+#             print(x)
+
+# apple = vedi()
+# apple.jk()
+
+# class vedi:
+#     def jk(self):
+#         print("hello")
+#         for x in range(2,60,3):
+#             print(x)
+#         else:
+#             print("earth")
+# com = vedi()
+# com.jk()
