3fad0f6fd671a3a48669576d57b807dec59f3d8c
Vedidhameliya11 <168515961+Vedidhameliya11@users.noreply.github.com>
2024-05-14 14:56:10 +0530

    Add files via upload

---
+++ vedi13.py
@@ -0,0 +1,19 @@
+# x = int(input("enter the number :"))
+
+# for i in range(2,x):
+#     if x%i == 0:
+#         print("not prime number")
+#         break
+#     else:
+#         print("prime number")
+#         break
+
+v = int(input("enter your number:"))
+
+for j in range(3,v):
+    if v%j == 0:
+        print("prime number")
+        break
+    else:
+        print("not prime number")
+        break
