9676d0eccfd34594757cf925aa223e17aeff0275
Vedidhameliya11 <168515961+Vedidhameliya11@users.noreply.github.com>
2024-05-16 14:48:28 +0530

    Add files via upload

---
+++ vedi15.py
@@ -0,0 +1,62 @@
+# class vedi:
+#     def __init__(jk,physics,chemistry):
+#         jk.physics = physics
+#         jk.chemistry = chemistry
+# data = vedi(90,95)
+# print(data.physics)
+# print(data.chemistry)
+
+# class v:
+#     def __init__(suga,jimin,jin,jhope):
+#         suga.jimin = jimin
+#         suga.jin = jin
+#         suga.jhope = jhope
+#     def hello():
+#         a = int(input())
+#         b = int(input())
+#         print(a*b)
+#     hello()
+# data = v(98,99,100)
+# print(data.jimin)
+# print(data.jin)
+# print(data.jhope)
+
+# class vedi:
+#     def __init__(lisa,e,c):
+#         lisa.e = e
+#         lisa.c = c
+# entry = vedi(95,100)
+# print(entry.e)
+# print(entry.c)
+
+# class southkorea:
+#     def __init__(seoul,busan,deagu):
+#         seoul.busan = busan
+#         seoul.deagu = deagu
+#     def jejuisland():
+#         j = int(input())
+#         k = int(input())
+#         print(j+k)
+#     jejuisland()
+# city = southkorea(5,4.5)
+# print(city.busan)
+# print(city.deagu)
+
+# class vedi:
+#     def jk(self):
+#         if 5>4:
+#             print("rgh")
+#         else:
+#             print("wrg")
+# apple = vedi()
+# apple.jk()
+
+class vedi:
+    def __init__(self):
+        if 7<6:
+            print("rgh")
+        else:
+            print("wrg")
+mango = vedi()
+mango.__init__()
+
